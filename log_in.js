698480f7d39f8e51e2b417cdc271d1d955fcb144
phantominor <motnahpminusone@gmail.com>
2023-01-15 15:47:14 +0800

    first commit

---
+++ log_in.js
@@ -0,0 +1,7 @@
+var btn_B = document.getElementById("btn.B")
+var btn_O = document.getElementById("btn.O")
+
+// btn_O.addEventListener("click", function(){
+//     onclick="location.href='https://mnya.tw';"
+// })
+
